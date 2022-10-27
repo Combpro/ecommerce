b052db08cc54a70d44dca6509fcb1b91795acb95
Rishabh Jain <rishuj.rj1995@gmail.com>
2022-10-27 20:33:06 +0530

    Add lazy loading, my profile page

--- client/src/data/product.js
+++ client/src/data/product.js
@@ -2,46 +2,37 @@ export const products = [
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-01.jpg",
         "h1": "Classic String",
-        "price": "₹499/-"
+        "qty": 2,
+        "price": 499
     },
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-02.jpg",
         "h1": "Classic String",
-        "price": "₹599/-"
+        "qty": 1,
+        "price": 599
     },
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-03.jpg",
         "h1": "Love Nana'20",
-        "price": "₹699/-"
+        "qty": 3,
+        "price": 699
     },
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-01.jpg",
         "h1": "Classic String",
-        "price": "₹799/-"
+        "qty": 1,
+        "price": 799
     },
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-02.jpg",
         "h1": "Classic String",
-        "price": "₹899/-"
+        "qty": 4,
+        "price": 899
     },
     {
         "img": "https://technext.github.io/hexashop/assets/images/men-03.jpg",
         "h1": "Love Nana'20",
-        "price": "₹999/-"
-    },
-    {
-        "img": "https://technext.github.io/hexashop/assets/images/men-01.jpg",
-        "h1": "Classic String",
-        "price": "₹1099/-"
-    },
-    {
-        "img": "https://technext.github.io/hexashop/assets/images/men-02.jpg",
-        "h1": "Air Force 1X",
-        "price": "₹399/-"
-    },
-    {
-        "img": "https://technext.github.io/hexashop/assets/images/men-03.jpg",
-        "h1": "Love Nana'20",
-        "price": "₹499/-"
-    },
+        "qty": 2,
+        "price": 999
+    }
 ]
